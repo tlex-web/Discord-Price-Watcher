00373806b1ac5cf6146d366aa631c1345f375e8f
tlex-web <asterix2514@gmail.com>
2022-04-01 22:14:22 +0200

    finalizing v1.0.0

--- getPrice.js
+++ getPrice.js
@@ -1,38 +1,35 @@
-require("dotenv").config()
+require('dotenv').config();
 const axios = require('axios').default;
 
-
 module.exports = {
-  async getPrice() {
-    let data
-      try {
-        response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
-        headers: {
-          'X-CMC_PRO_API_KEY': process.env.CMC_KEY,
-        },
-      });
-      } 
-      catch (error) {
-        if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an     instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-      }
-      console.log(error.config);
-      }
-      if (response) {
-        const json = response.data;
-        console.log(json);
-      }
-  }
-  }
+	async getPrice() {
+		let response;
+		try {
+			response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=LIBERO', {
+				headers: {
+					'X-CMC_PRO_API_KEY': process.env.CMC_KEY,
+				},
+			});
+		} catch (error) {
+			if (error.response) {
+				// The request was made and the server responded with a status code
+				// that falls out of the range of 2xx
+				console.log(error.response.data);
+				console.log(error.response.status);
+				console.log(error.response.headers);
+			} else if (error.request) {
+				// The request was made but no response was received
+				// `error.request` is an instance of XMLHttpRequest in the browser and
+				// an instance of http.ClientRequest in node.js
+				console.log(error.request);
+			} else {
+				// Something happened in setting up the request that triggered an Error
+				console.log('Error', error.message);
+			}
+			console.log(error.config);
+		}
+		if (response) {
+			return response.data.data.LIBERO[0];
+		}
+	},
+};
--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const { Client, Intents } = require("discord.js")
-require("dotenv").config()
+const { Client, Intents } = require('discord.js');
+require('dotenv').config();
 
-//const { getPrice } = require('./getPrice.js')
-const runner = require('./server.js')
+const { getPrice } = require('./getPrice.js');
+const runner = require('./server.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on('ready', async () => {
+	console.log(`Logged in as ${client.user.tag} !`);
+
+	const message = await client.channels.fetch('937697459369566229');
+	//message.send("I'm listening to $LIBERO price movements");
+
+	setInterval(async () => {
+		const price = await getPrice();
+		await message.guild.me.setNickname(`${price.quote.USD.price.toPrecision(5)} $`);
+	}, 10000);
+
+	client.user.setActivity('Price Movements', { type: 'WATCHING' });
 });
 
-console.log(true)
-
-// const member = client.users.fetch(959411283407548456)
-
-// member.then(function(res) {
-//     console.log(res)
-// })
-runner()
+runner();
 
 client.login(process.env.TOKEN);
